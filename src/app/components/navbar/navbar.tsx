e5c71773d564f414aa4c261d6f2d046337aa52ff
Benjamin Wesson <benjamin.j.wesson@gmail.com>
2025-10-09 19:58:02 -0400

    translations for navbar

--- src/app/components/navbar/navbar.tsx
+++ src/app/components/navbar/navbar.tsx
@@ -11,24 +11,27 @@ import {
 } from "@/components/ui/select";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/utils/authOptions";
+import { getTranslations } from "next-intl/server";
+
 
 export default async function Navbar({ locale }: { locale: string }) {  
   const session = await getServerSession(authOptions);
+  const t = await getTranslations("Navbar");
 
   return (
     <div className={styles.container}>
       <div className={styles.links}>
         <div>
-          <Link className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white" href="/">Account</Link>
+          <Link className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white" href="/">{t("account")}</Link>
         </div>
         <div>
-          <Link className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white" href="/funds">Funds</Link>
+          <Link className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white" href="/funds">{t("funds")}</Link>
         </div>
         <div>
-          <Link className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white"   href="/buy">Buy</Link>
+          <Link className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white"   href="/buy">{t("buy")}</Link>
         </div>
         <div>
-          <Link className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white" href="/sell">Sell</Link>
+          <Link className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white" href="/sell">{t("sell")}</Link>
         </div>
         <div>
           <AuthLink />
