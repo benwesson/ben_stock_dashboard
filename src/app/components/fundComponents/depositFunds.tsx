5182e0b48e1b84baf4455a23808687b889b966ca
Benjamin Wesson <benjamin.j.wesson@gmail.com>
2025-10-10 21:20:58 -0400

    translations for template

--- src/app/components/fundComponents/depositFunds.tsx
+++ src/app/components/fundComponents/depositFunds.tsx
@@ -1,6 +1,5 @@
-"use client";
 
-import { addFunds } from "@/actions/prisma_api";
+import { getTranslations } from "next-intl/server";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -12,30 +11,28 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-
-export default function DepositFunds() {
+export default async function DepositFunds() {
+  const t = await getTranslations("DepositFunds");
   return (
     <>
       <Card className="mt-8">
         <CardHeader>
-          <CardTitle>Deposit Funds</CardTitle>
-          <CardDescription>Add funds to your account</CardDescription>
+          <CardTitle>{t("title")}</CardTitle>
+          <CardDescription>{t("description")}</CardDescription>
         </CardHeader>
         <CardContent>
           <form>
             <div className="flex gap-2">
               <Input
               type="number"
-              placeholder="Amount"
+              placeholder={t("amountPlaceholder")}
               name="amount"
               min={0.01}
               max={1000000}
               required
             />
-            <Button type="submit">Deposit</Button>
-
-            </div>
-            
+            <Button type="submit">{t("depositButton")}</Button>
+            </div>  
           </form>
         </CardContent>
       </Card>
